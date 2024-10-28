2e3bcbd43577211db41e3800558b0ea2c12f66d6
Zhichen <qhwuzhichen@126.com>
2024-10-28 22:37:32 +0800

    223
    223

--- website/build/pdf.js
+++ website/build/pdf.js
@@ -12019,7 +12019,7 @@ class PDFWorker {
   static {
     if (isNodeJS) {
       this.#isWorkerDisabled = true;
-      GlobalWorkerOptions.workerSrc ||= "./pdf.worker.mjs";
+      GlobalWorkerOptions.workerSrc ||= "./pdf.worker.js";
     }
     this._isSameOrigin = (baseUrl, otherUrl) => {
       let base;
